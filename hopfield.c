1063c9dfe26d7f3b348443c6db68109874134ef1
Ramón <ramonmr@correo.ugr.es>
2021-05-11 13:13:30 +0200

    Creado inicialización de imagen

--- hopfield.c
+++ hopfield.c
@@ -3,10 +3,12 @@
 #include <time.h>
 #include "gsl_rng.h"
 
-#define NUM 100
-#define NIM 1
+#define NUM 100 //Número de neuronas
+#define NIM 1 //Número de imágenes recordadas
 
 gsl_rng *tau; //Variable para los num aleatorios
+
+void iniImagen(int imagen[][NIM]);
 
 int main(){
     
@@ -17,6 +19,22 @@ int main(){
     tau = gsl_rng_alloc(gsl_rng_taus);
     gsl_rng_set(tau,semilla);
     
+    //Declaración variables
+    int imagen[NUM][NIM], spines[NUM][NIM];
+    FILE *f1;
+    
+    f1 = fopen("imagen.txt","w");
+    
+    iniImagen(imagen);
+    for(int i=0;i<NUM;i++) fprintf(f1,"%i\t",imagen[i][0]);
+    fclose(f1);
     
 }
 
+void iniImagen(int imagen[][NIM]){
+    for(int i=0;i<NUM;i++){
+        if(i%2==0){
+            imagen[i][0] = 0;
+        }else imagen[i][0] = 1;
+    }
+}
