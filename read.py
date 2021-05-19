c64ef5837b235c86d40a612c303fda1d524dc8ef
Ramón <ramonmr@correo.ugr.es>
2021-05-19 17:19:22 +0200

    Se añade el codigo python para pasar imagenes a arrays

---
+++ read.py
@@ -0,0 +1,25 @@
+import numpy as np
+import imageio
+
+image = imageio.imread('blanco.png')
+
+alto = len(image[:,0]) #Altura imagen
+ancho = len(image[0]) #Anchura imagen
+
+f = open('.imtemp.txt', 'w')
+
+for x in range(alto):
+    for y in range(ancho):
+        r = image[x][y][0]
+        g = image[x][y][1]
+        b = image[x][y][2]
+        
+        g = 0.299*r + 0.587*g + 0.114*b
+        
+        if g < 255/2:
+            g = 0
+        else:
+            g = 1
+            
+        f.write(str(g))
+        f.write(' ')
